9f96992050641f7dabadcf44490411c51cbcdceb
YudiZulayMartinezSuarez <yzmartinez@uts.edu.co>
2023-03-11 14:47:14 -0500

    feat:Ejercicio(2)

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
-let nombre = prompt("nombre: ");
+let numero = prompt("digite un numero: ");
 
-let nota1 = parseFloat(prompt("nota1: "));
-let nota2 = parseFloat(prompt("nota2: "));
-let nota3 = parseFloat(prompt("nota3: "));
+let resultado = ImparPar(numero);
+alert("el numero "+numero+ " es " +resultado);
 
-let maximo = (nota1 +nota2 +nota3)/3;
-
-if (maximo>=3.9){
-    console.log("su nota es : " +maximo+ " becado");
-}else{
-    console.log("su nota es : " +maximo+ " estudie ");
+function ImparPar(numero){
+    if(numero % 2 == 0){
+        return "Par";
+    }else {
+        return "Impar";
+    }
 }
